bce20038a62ab34ab51ee401af4503a33a11785a
Richard Lu <richard_lu@haas.berkeley.edu>
2019-06-05 11:44:06 -0700

    fix some bugs and finish breaking up data

--- 1_data_processing/break_up_reviews_into_smaller_datasets.py
+++ 1_data_processing/break_up_reviews_into_smaller_datasets.py
@@ -4,7 +4,7 @@
 # Description:
 #   review.json is too large (~6GB) to load directly using pd.read_json
 #   break up the file into smaller files for reading
-# Runtime: 
+# Runtime: ~20s per 1M rows
 # =========================================================================== #
 
 import os
@@ -19,11 +19,11 @@ from collections import defaultdict
 # =========================================================================== #
 
 data_folder = "../0_data/1_raw_data"
-output_data_folder = "../0_data/2_processed_data/reviews_by_business_id"
+output_data_folder = "../0_data/2_processed_data/reviews_by_business_id/tmp"
 filename = "review.json"
 
-starting_index = sys.argv[1]
-ending_index = sys.argv[2]
+starting_index = int(sys.argv[1])
+ending_index = int(sys.argv[2])
 file_counter = sys.argv[3]
 
 data_dict = defaultdict(list)
@@ -38,8 +38,8 @@ with open(os.path.join(data_folder, filename), "r") as f:
                 print(counter)
                 tmp_review = ujson.loads(line)
                 tmp_business_id = tmp_review["business_id"]
-                data_dict["tmp_business_id"].append(tmp_review)
-            else: # to catch when the data "runs" out
+                data_dict[tmp_business_id].append(tmp_review)
+            except: # to catch when the data "runs" out
                 continue
         counter += 1
 
