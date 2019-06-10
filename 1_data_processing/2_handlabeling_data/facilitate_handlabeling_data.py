064c22f0104606c29f782549bd601e81e67e1ef1
Richard Lu <richard_lu@haas.berkeley.edu>
2019-06-10 09:48:08 -0700

    increase number of sampled reviews

--- 1_data_processing/2_handlabeling_data/facilitate_handlabeling_data.py
+++ 1_data_processing/2_handlabeling_data/facilitate_handlabeling_data.py
@@ -67,7 +67,7 @@ except:
 # randomization method (i.e. checking if it is already labeled then
 # randomly selecting another)
 
-random_reviews = select_reviews(random_seed, 1000)
+random_reviews = select_reviews(random_seed, 10000)
 
 counter = 0
 
